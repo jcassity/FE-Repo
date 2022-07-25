c291bd62d1f2eb27e41e64c388d08dc2fda6a438
jcassity <jacobcassity123@gmail.com>
2022-07-25 15:08:05 -0500

    updated bat files

---
+++ ZZ Repo Updater.bat
@@ -0,0 +1,9 @@
+f/github/fe-repo
+
+npm run battle-animations-readme
+npm run bg-interface-readme
+npm run class-cards-readme
+npm run item-icons-readme
+npm run maps-readme
+npm run map-sprites-readme
+npm run portraits-readme
